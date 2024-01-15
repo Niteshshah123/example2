3aafd9e9789c0f383b7cd2eab3247a1272555755
Niteshshah123 <niteshshah011@gmail.com>
2024-01-16 00:16:08 +0545

    modify the new java file

---
+++ javaexample.java
@@ -0,0 +1,8 @@
+package example2;
+
+public class javaexample {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+    }
+}
